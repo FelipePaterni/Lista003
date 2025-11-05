a5c5f691123ab5c9076a0b8c61b9d0fb4a5fea12
Felipe <felipesp.chaves@gmail.com>
2025-11-05 16:04:08 -0300

    5

---
+++ 5/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        Lista003 l1 = new Lista003();
+        Lista003 l2 = new Lista003();
+
+        while (true) {
+            int v = sc.nextInt();
+            if (v == -1) break;
+            l1.append(new Node<>(v));
+        }
+
+        while (true) {
+            int v = sc.nextInt();
+            if (v == -1) break;
+            l2.append(new Node<>(v));
+        }
+
+        Lista003 inter = (Lista003) l1.repItens(l2);
+
+        Node curr = inter.getHead();
+        while (curr != null) {
+            System.out.println(curr.getData());
+            curr = curr.getNext();
+        }
+    }
+}
